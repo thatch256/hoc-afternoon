fde9ca0bb5fa444dd8c67ee84435cab478f916b9
thatch256 <thatch256@gmail.com>
2019-09-02 16:58:48 -0600

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CurrencyHOC from './Components/CurrencyConverter'
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <>
+        <h2>Higher Order Component</h2>
+        <CurrencyHOC />
+      </>
     );
   }
 }
